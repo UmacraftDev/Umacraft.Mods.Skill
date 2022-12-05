2d47c051e29ce282da5d26c2d6c47a4c120c79e5
Yang <xray_yang@foxmail.com>
2022-12-05 15:36:18 +0800

    Changed LICENSE and Removed TestSkill
    Signed-off-by: Yang <xray_yang@foxmail.com>

--- src/main/kotlin/cn/umacraft/mods/skill/skill/green/road/13BuffSkill.kt
+++ src/main/kotlin/cn/umacraft/mods/skill/skill/green/road/13BuffSkill.kt
@@ -1,3 +1,27 @@
+/*
+ * Umacraft.Mods.Skill - A Skill mod for Umacraft Minecraft Server
+ * https://github.com/UmacraftDev/Umacraft.Mods.Skill
+ * Copyright (C) 2021-2022  UmacraftDev
+ *
+ * This Library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not,
+ * see <https://www.gnu.org/licenses/>.
+ *
+ * Please contact us by email xray_yang@foxmail.com
+ * if you need additional information or have any questions
+ */
 package cn.umacraft.mods.skill.skill.green.road
 
 import cn.umacraft.mods.skill.skill.FatherSkill
